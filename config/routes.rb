1f0a1c62af17f0f9d226e555ef09967cc5aa48c2
samer <mrsamer123@hotmail.com>
2017-01-23 11:04:26 +0000

    Added Messages System

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   root to: 'pages#home'
 
   resources :high_scores
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122091226) do
+ActiveRecord::Schema.define(version: 20170123104727) do
 
   create_table "high_scores", force: :cascade do |t|
     t.string   "game"
     t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
