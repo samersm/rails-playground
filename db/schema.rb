d1956b10016b54e6c481604783c047b5f15d6c45
samer <mrsamer123@hotmail.com>
2017-01-23 12:37:27 +0000

    Tested some db commands

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123104727) do
+ActiveRecord::Schema.define(version: 20170123122533) do
 
   create_table "high_scores", force: :cascade do |t|
     t.string   "game"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170123104727) do
     t.string   "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
